c79dcf500f8720fadf878ea997ea7d9aee1747be
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-07-27 17:34:41 -0400

    environment variable

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
-  AWS_REGION  = "us-east-1"
-  AWS_PROFILE = "default"
+  default  = "us-east-1"
+  
 }
