d07f26ad47b710982e3af14101205f33920f25ec
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-07-27 11:52:41 -0400

    files updated

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_api_gateway_rest_api" "example" {
 resource "aws_api_gateway_resource" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part    = "example"
+  path_part   = "example"
 }
 
 # API Gateway Method
@@ -101,28 +101,39 @@ resource "aws_api_gateway_method" "example" {
 
 # API Gateway Integration
 resource "aws_api_gateway_integration" "example" {
-  rest_api_id             = aws_api_gateway_rest_api.example.id
-  resource_id             = aws_api_gateway_resource.example.id
-  http_method             = aws_api_gateway_method.example.http_method
-  integration_http_method = "GET"
-  type                    = "HTTP"
-  uri                     = aws_lambda_function_url.resume_lambda_url.function_url
-}
-
+    rest_api_id = aws_api_gateway_rest_api.example.id
+    resource_id = aws_api_gateway_resource.example.id
+    http_method = aws_api_gateway_method.example.http_method
+    type        = "HTTP"
+    uri         = aws_lambda_function_url.resume_lambda_url.function_url
+    integration_http_method = "GET"
+}  
+ 
 # Lambda Permission for API Gateway
 resource "aws_lambda_permission" "allow_api_gateway" {
+
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.resume_lambda.function_name
   principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
 }
 
 # API Gateway Deployment
 resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  stage_name   = "prod"
+  description = "Example deployment"
+  stage_description = "Example deployment"
 
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  stage_name = "prod"
   depends_on = [
+    aws_api_gateway_method.example,
     aws_api_gateway_integration.example
   ]
 }
--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,14 @@ output "lambda_function_url" {
   value = aws_lambda_function_url.resume_lambda_url.function_url
 }
 
+/*
 output "resume_items_raw" {
   value = file("${path.module}/resume.json")
 }
+*/
 
 output "api_url" {
   description = "The URL of the API Gateway endpoint"
-  value = "https://${aws_api_gateway_rest_api.example.id}.execute-api.${var.aws_region}.amazonaws.com/prod/ResumeFunction"
+  value       = "https://${aws_api_gateway_rest_api.example.id}.execute-api.${var.aws_region}.amazonaws.com/prod/ResumeFunction"
 }
-  
+
