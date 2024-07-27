05fddb12f4bfd61a3ced044de2875343cde263ea
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-07-27 15:41:14 -0400

    plan github

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-variable "github_token" {
-  description = "GitHub personal access token"
-  type        = string
-  sensitive   = true
-}
-
-variable "github_owner" {
-  description = "GitHub username or organization"
-  type        = string
-}
-
-variable "repo_name" {
-  description = "The name of the GitHub repository"
-  type        = string
-}
-
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
