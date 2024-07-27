e5f32ac91d91a58d41e285790372a1a54657bfc1
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-07-27 17:13:22 -0400

    secrets

--- variables.tf
+++ variables.tf
@@ -2,4 +2,9 @@ variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
   default     = "us-east-1"
+  AWS_REGION  = "us-east-1"
+  AWS_PROFILE = "default"
+  AWS_ACCESS_KEY_ID = ""
+  AWS_SECRET_ACCESS_KEY = ""
+
 }
